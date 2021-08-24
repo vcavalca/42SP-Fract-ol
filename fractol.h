f72e7532d27ce62ff867aeb0851db6606ea6a7f9
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-08-24 14:50:27 -0300

    Update

---
+++ fractol.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 12:30:40 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/08/24 14:48:15 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# include "minilibx-linux/mlx.h"
+
+#endif
