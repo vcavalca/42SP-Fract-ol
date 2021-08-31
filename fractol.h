4935036dd6d8682d61275c867f4e8f71508a93c5
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-08-31 16:42:19 -0300

    Made a struct named mlx and defined window size

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 12:30:40 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/08/24 14:48:15 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/08/31 16:40:53 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,19 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <math.h>
 # include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
+
+/*window size*/
+# define WIN_X 1024
+# define WIN_Y 768
+
+/*Struct MLX*/
+typedef struct s_mlx
+{
+	void	*mlx;
+	void	*mlx_win;
+}t_mlx;
 
 #endif
