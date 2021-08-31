45b95f6ed75142f2c53dfa1fd249f8eafc6bdcd5
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-08-31 15:52:54 -0300

    Exemple Up

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 14:31:40 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/08/24 14:44:44 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/08/31 15:50:45 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int	main(void)
 {
-	return (0);
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 800, 600, "Hello World!");
+	mlx_loop(mlx);
 }
