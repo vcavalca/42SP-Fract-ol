cf930dd1135552257ff1e3e5ea11deb69d4ae73f
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-08-31 17:41:42 -0300

    Change name

--- fractol.c
+++ fractol.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 14:31:40 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/08/31 15:50:45 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/08/31 17:41:15 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ int	main(void)
 	void	*mlx_win;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 600, "Hello World!");
+	mlx_win = mlx_new_window(mlx, WIN_X, WIN_Y, "Fractol");
 	mlx_loop(mlx);
 }
