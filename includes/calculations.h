0f2ac2b109cb3ca18dc0f7e0cf4ce4db30462696
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-09-02 20:48:00 -0300

    Lib with sames values for calculations

---
+++ includes/calculations.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calculations.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 17:22:09 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/09/02 17:23:48 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CALCULATIONS_H
+# define CALCULATIONS_H
+
+/* calc */
+# define DIV_VALUE       2
+# define REDUCED_SCALE   100.0
+# define INIT_X_POSITION -3.0
+# define INIT_Y_POSITION -3.0
+# define MOVE_DIST       0.5
+# define INIT_MAG        1.0
+# define MAGNIFICATION   0.5
+# define INIT_IMAX       25
+# define MAX_IMAX        100
+# define CHANGE_IMAX     5
+
+#endif
