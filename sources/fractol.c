7b745918ab7f5d5c21576aec5a48d0b8106ac2c6
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-09-02 20:53:05 -0300

    Update

--- sources/fractol.c
+++ sources/fractol.c
@@ -6,19 +6,104 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 14:31:40 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/09/02 10:27:34 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/09/02 20:14:05 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int	main(int argc, char **argv)
+int	ft_make_img(t_fractol *fractol)
+{
+	fractol->img = mlx_new_image(fractol->mlx, SCREEN_X, SCREEN_Y);
+	if (fractol->img == NULL)
+	{
+		ft_putendl_fd(ERROR_INIT_IMG, 2);
+		return (1);
+	}
+	fractol->addr = mlx_get_data_addr(fractol->img, &fractol->bits_per_pixel,
+			&fractol->size_line, &fractol->endian);
+	return (0);
+}
+
+int	ft_fractol_init(t_fractol *fractol)
+{
+	fractol->color = INIT_COLOR;
+	fractol->side = INIT_X_POSITION;
+	fractol->vert = INIT_Y_POSITION;
+	fractol->mag = INIT_MAG;
+	fractol->max_i = INIT_IMAX;
+	fractol->mlx = mlx_init();
+	if (fractol->mlx == NULL)
+		return (1);
+	fractol->mlx_win = mlx_new_window(fractol->mlx,
+			SCREEN_X, SCREEN_Y, "Fractol");
+	if (fractol->mlx_win == NULL)
+		return (1);
+	return (0);
+}
+
+int	ft_argv_check(char *s)
 {
 	int	i;
 
-	if (argc >= 2 && argv <= 11)
+	i = 0;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	while (s[i] && s[i] != '.')
 	{
-		i = 1;
+		if (!ft_isdigit(s[i]))
+			return (0);
+		i++;
 	}
+	if (s[i])
+		i++;
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_args_verification(t_fractol *fractol, int argc, char **argv)
+{
+	if (argc == 2 && (!ft_strcmp_f(argv[1], "1") || !ft_strcmp_f(argv[1], "2")
+			|| !ft_strcmp_f(argv[1], "4")))
+		fractol->type = argv[1][0] - '0';
+	else if (argc == 4 && !ft_strcmp_f(argv[1], "3") && ft_argv_check(argv[2])
+		&& ft_argv_check(argv[3]))
+	{
+		fractol->type = argv[1][0] - '0';
+		fractol->x_param = ft_atof(argv[2]);
+		fractol->y_param = ft_atof(argv[3]);
+	}
+	else
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_fractol	fractol;
+
+	ft_bzero(&fractol, 17);
+	if (ft_args_verification(&fractol, argc, argv))
+	{
+		ft_error_print();
+		return (1);
+	}
+	if (ft_fractol_init(&fractol) || ft_make_img(&fractol))
+	{
+		ft_free_mlx(&fractol);
+		return (1);
+	}
+	ft_fractal(&fractol);
+	mlx_mouse_hook(fractol.mlx_win, ft_mouse_click, &fractol);
+	mlx_hook(fractol.mlx_win, 6, 1L << 6, ft_mouse_hook, &fractol);
+	mlx_hook(fractol.mlx_win, 2, 1L << 0, ft_key_hook, &fractol);
+	mlx_hook(fractol.mlx_win, BUTTON_CROSS, 1L << 17,
+		ft_cross_button, &fractol);
+	mlx_loop(fractol.mlx);
 	return (0);
 }
--- sources/utils.c
+++ sources/utils.c
@@ -6,16 +6,90 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 17:55:52 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/09/02 10:31:36 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/09/02 18:54:44 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-void	my_mlx_pixel_put(t_img_data	*img, int x, int y, int color)
+int	ft_strcmp_f(const char *s1, const char *s2)
+{
+	int	i;
+
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (1);
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+double	ft_atof(char *s)
+{
+	double	sign;
+	double	n;
+	double	i;
+
+	sign = 1;
+	n = 0;
+	i = 1;
+	if (*s == '+')
+		s++;
+	else if (*s == '-')
+	{
+		s++;
+		sign = -1;
+	}
+	while (ft_isdigit(*s))
+	{
+		n = n * 10 + *s - '0';
+		s++;
+	}
+	if (*s == '.')
+	{
+		while (ft_isdigit(*++s))
+			n += (*s - '0') * (i *= 0.1);
+	}
+	return (n * sign);
+}
+
+void	ft_free_mlx(t_fractol *fractol)
+{
+	mlx_destroy_image(fractol->mlx, fractol->img);
+	mlx_destroy_window(fractol->mlx, fractol->mlx_win);
+	mlx_destroy_display(fractol->mlx);
+	free(fractol->mlx);
+}
+
+void	ft_error_print(void)
+{
+	ft_putstr("Please, Give a number as an argument");
+	ft_putstr("\n");
+	ft_putstr(NULL);
+	ft_putstr("You can specify from 1 to 4 fractals");
+	ft_putstr("\n");
+	ft_putstr(NULL);
+	ft_putstr("List of available fractals:");
+	ft_putstr("\n");
+	ft_putstr("1 : Mandelbrot");
+	ft_putstr("\n");
+	ft_putstr("2 : Julia with mouse coordinate");
+	ft_putstr("\n");
+	ft_putstr("3 : Julia with params");
+	ft_putstr("\n");
+	ft_putstr("4 : Burning Ship");
+	ft_putstr("\n\n");
+}
+
+void	my_mlx_pixel_put(t_fractol *fractol, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = img->addr + (y * img->size_length + x *(img->bits_per_pixel / 8));
+	dst = fractol->addr + (y * fractol->size_line
+			+ x * (fractol->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
