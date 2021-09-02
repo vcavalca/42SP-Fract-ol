e546032021c7ce26aaaa841cc060453dbced9e15
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-09-02 06:28:56 -0300

    Update

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,41 +6,47 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 12:30:40 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/08/31 20:43:39 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/09/02 06:25:27 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
 # include <math.h>
+# include <stdint.h>
 # include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
+# include "colors.h"
+# include "error_messages.h"
+# include "key_linux.h"
 
-/*window size*/
-# define WIN_X 1024
-# define WIN_Y 768
+/*Screen resolution*/
+# define SCREEN_X 1024
+# define SCREEN_Y 768
 
-/*Struct IMG*/
+/*Struct Image MLX*/
 typedef struct s_img_data
 {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
-	int		line_length;
+	int		size_length;
 	int		endian;
 }t_img_data;
 
-/*Struct MLX*/
+/*Struct Fractol MLX*/
 typedef struct s_mlx
 {
-	void	*mlx;
-	void	*mlx_win;
+	void		*mlx;
+	void		*mlx_win;
+	t_img_data	*img;
 }t_mlx;
 
-void	my_mlx_pixel_put(t_img_data	*img, int x, int y, int color);
+void		my_mlx_pixel_put(t_img_data	*img, int x, int y, int color);
+void		*ft_memmalloc(size_t size);
+void		ft_exit(char *s);
+
+t_img_data	*ft_init_img(void *mlx);
 
 #endif
