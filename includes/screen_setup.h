22fe87a64e7f3cc628a06e4bb132a4f9bdcdc815
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-09-02 06:31:22 -0300

    Update

---
+++ includes/screen_setup.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   screen_setup.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 06:02:52 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/09/02 06:30:36 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCREEN_SETUP_H
+# define SCREEN_SETUP_H
+
+# define COLOR_SILVER 0xCCCCCC
+# define COLOR_TEST 0x00FF0000
+
+#endif
